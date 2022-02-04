ec291f57a6d5acd1d5326880e39982ee7530c905
carlos rodriguez <twincarlos98@gmail.com>
2022-02-04 11:31:17 -0500

    fixed dropdown menu button

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -30,7 +30,7 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button className={showMenu ? 'dropdown-active' : 'dropdown-not-active'} onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
       {showMenu && (

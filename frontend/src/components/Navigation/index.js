545fc0bb8c74fa89d4fd695bb243c8d45c771d87
carlos rodriguez <twincarlos98@gmail.com>
2022-02-05 08:19:43 -0500

    demo user feature added and completed

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import BookingSearchBar from '../BookingSearchBar/BookingSearchBar';
+import * as sessionActions from "../../store/session";
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+
+  const loginDemo = () => {
+    return dispatch(sessionActions.login({ credential: 'demo@user.io', password: 'password' }));
+  }
 
   let sessionLinks;
   if (sessionUser) {
@@ -19,6 +25,7 @@ function Navigation({ isLoaded }){
       <>
         <LoginFormModal />
         <NavLink id='navlink' to="/signup">Sign Up</NavLink>
+        <button id='demo-login' type='submit' onClick={() => loginDemo()}>Demo</button>
       </>
     );
   }

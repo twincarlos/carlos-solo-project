81731bc254a35b42ba8644feddea069180588479
carlos rodriguez <twincarlos98@gmail.com>
2022-02-13 03:31:47 -0500

    nav bar fully operational

--- frontend/src/components/SpotsGallery/StateList.js
+++ frontend/src/components/SpotsGallery/StateList.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { getAllSpots } from '../../store/spot';
 import { BookingContext } from '../../context/BookingContext';
 
@@ -9,6 +9,7 @@ function StateList () {
     const dispatch = useDispatch();
     const { setLocation } = useContext(BookingContext);
     const spotList = useSelector((state) => Object.values(state.spot));
+    const [more, setMore] = useState(false);
 
     useEffect(() => {
         dispatch(getAllSpots());
@@ -16,9 +17,8 @@ function StateList () {
 
     return (
         <ul id='states-nav'>
-            <li>All</li>
-            { spotList && spotList.map((spot, i) => (i < 10) && (<li key={i} onClick={() => setLocation(spot.state)}>{spot.state}</li>))}
-            <li>Filters</li>
+            <li onClick={() => setMore(!more)}>{more ? 'Hide' : 'More'}</li>
+            {spotList.map((spot, idx) => (more || idx < 10) ? <li key={`${spot.id}`} onClick={() => setLocation(spot.state)}>{spot.state}</li> : null)}
         </ul>
     );
 }
